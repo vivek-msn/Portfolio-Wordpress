b1293f00cb9fc2ca316e6da526f1375bc4787cde
vivek-msn <vivek_msn@hotmail.com>
2024-06-11 16:52:30 +0530

    add custom logo

--- functions.php
+++ functions.php
@@ -22,3 +22,19 @@
     }
 
     add_action( 'wp_enqueue_scripts', 'portfolio_scripts' );
+
+
+    function portfolio_config_setup(){
+        add_theme_support('custom-logo',array(
+            'height'        => 100,
+            'width'         => 400,
+            'flex-height'   => true,
+            'flex-width'    => true,
+        )  );
+        add_theme_support('title-tag');
+        add_theme_support('post-thumbnails');
+    }
+
+    add_action('after_setup_theme', 'portfolio_config_setup');
+
+    ?>
